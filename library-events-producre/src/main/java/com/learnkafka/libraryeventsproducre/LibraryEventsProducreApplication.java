6763ea7cfdd6595fdbb8b2d78df744519168dfb3
rahman <margub.9363@gmail.com>
2023-03-05 10:02:22 +0530

    Basic Project Setup

---
+++ library-events-producre/src/main/java/com/learnkafka/libraryeventsproducre/LibraryEventsProducreApplication.java
@@ -0,0 +1,13 @@
+package com.learnkafka.libraryeventsproducre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryEventsProducreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryEventsProducreApplication.class, args);
+	}
+
+}
---
+++ library-events-producre/src/test/java/com/learnkafka/libraryeventsproducre/LibraryEventsProducreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnkafka.libraryeventsproducre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryEventsProducreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
