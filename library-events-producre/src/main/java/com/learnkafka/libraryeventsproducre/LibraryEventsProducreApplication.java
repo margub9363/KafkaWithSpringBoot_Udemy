f3eba41e1e79917af5a9a786bb83412e44f7b800
rahman <margub.9363@gmail.com>
2023-03-05 10:32:49 +0530

    L-24 Post Method and Component Scan

--- library-events-producre/src/main/java/com/learnkafka/libraryeventsproducre/LibraryEventsProducreApplication.java
+++ library-events-producre/src/main/java/com/learnkafka/libraryeventsproducre/LibraryEventsProducreApplication.java
@@ -2,8 +2,10 @@ package com.learnkafka.libraryeventsproducre;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.learnkafka")
 public class LibraryEventsProducreApplication {
 
 	public static void main(String[] args) {
