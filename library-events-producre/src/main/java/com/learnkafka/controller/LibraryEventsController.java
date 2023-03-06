c981e7d1532631cb00ed4a0d5e5c95a79b07a373
rahman <margub.9363@gmail.com>
2023-03-06 08:55:48 +0530

    L-31 Approach 2 i.e synchronous calls for Kafka

--- library-events-producre/src/main/java/com/learnkafka/controller/LibraryEventsController.java
+++ library-events-producre/src/main/java/com/learnkafka/controller/LibraryEventsController.java
@@ -3,25 +3,34 @@ package com.learnkafka.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.learnkafka.domain.LibraryEvent;
 import com.learnkafka.producer.LibraryEventProducer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+
+@Slf4j
 @RestController
 public class LibraryEventsController {
 
     @Autowired
     LibraryEventProducer libraryEventProducer;
     @PostMapping("/v1/libraryevent")
-    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException {
+    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException, ExecutionException, InterruptedException, TimeoutException {
 //        invoke kafka producer
-        libraryEventProducer.sendLibraryEvent(libraryEvent);
+        log.info("before Sending Library Event");
+        SendResult<Integer, String> sendResult = libraryEventProducer.sendLibraryEventSynchronous(libraryEvent);
+        log.info("Send Result is {} ",sendResult.toString());
+        log.info("after sendLibraryEven");
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
 
